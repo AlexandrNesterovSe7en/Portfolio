bf23c4be39cb42f7d5c7e063273384f59fa0b8c4
Alexandr Nesterov <pudj.nesterov2004@mail.ru>
2023-07-17 12:07:39 +0800

    set up theme light dark

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./Routes/routes";
-
+import './ThemeSite/theme.css';
 
 const router = createBrowserRouter(routes)
 
