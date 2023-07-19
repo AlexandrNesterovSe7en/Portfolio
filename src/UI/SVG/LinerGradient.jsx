a0fc0bf46a47e804d4d40e2edf651469faa08db5
Alexandr Nesterov <pudj.nesterov2004@mail.ru>
2023-07-19 11:34:39 +0800

    fix un correct properties stop-color -> stopColor и т.д. для SVG

--- src/UI/SVG/LinerGradient.jsx
+++ src/UI/SVG/LinerGradient.jsx
@@ -2,8 +2,8 @@ const LinerGradient = () => {
     return (
         <defs>
         <linearGradient id="MyGradient">
-          <stop offset="0%" stop-color="#13B0F5" />
-          <stop offset="100%" stop-color="#E70FAA" />
+          <stop offset="0%" stopColor="#13B0F5" />
+          <stop offset="100%" stopColor="#E70FAA" />
         </linearGradient>
       </defs>
     );
